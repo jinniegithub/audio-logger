f62fc7e22780bd82f1aa62518f9c3c5eafb31b81
jinniegithub <jintian1984@gmail.com>
2017-04-22 00:18:51 +0800

    Change popover to toast.

--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -2,7 +2,7 @@ import { NoteItem } from './noteitem';
 import { FootInfoPage } from '../footinfopage/footinfopage';
 import { PopOverMsg } from '../popovermsg/popovermsg';
 import { Component } from '@angular/core';
-import { PopoverController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 
 @Component({
@@ -12,21 +12,23 @@ import { PopoverController } from 'ionic-angular';
 export class Notes {
     items : Array<NoteItem>;
 
-  constructor(private popoverCtrl: PopoverController) {
+  constructor(private toastCtrl: ToastController) {
       this.items = [];
 
   }
 
+  presentToast() {
+      let toast = this.toastCtrl.create({
+          message: 'New log was added successfully',
+          duration: 3000,
+          position: 'middle',
+      });
+      toast.present();
+  } 
+
   addItem(event) {
       this.items.push(event);
       console.log("Notes Component Add " + event);
+      this.presentToast();
   }
-  presentPopover(ev) {
-
-    let popover = this.popoverCtrl.create(PopOverMsg);
-
-    popover.present({
-      ev: ev
-    });
-  } 
 }
